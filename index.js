7591781f9c65194e21e257f1dce2afbc640a9858
koneko <67551503+koneko@users.noreply.github.com>
2022-06-15 22:38:30 +0200

    finished game

--- index.js
+++ index.js
@@ -11,5 +11,120 @@ let server = app.listen(port, () => console.log("TicTacToe server listening on p
 const io = new Server(server);
 
 io.on('connection', socket => {
-    console.log(`${socket.id} connected.`);
+    socket.on('join', id => {
+        console.log(`${socket.id} wishes to join room ${id}.`)
+        let room = rooms.find(r => r.id == id)
+        if (room == null) {
+            console.log("Room does not exist, creating room.")
+            rooms.push(createRoom(id, socket))
+            socket.emit('roomResponse', { response: true, sign: "X" })
+            socket.join(id)
+        } else {
+            if (room.players.length > 1) return socket.emit('roomResponse', { response: false })
+            room.players.push({ sign: 'O', socket: socket })
+            socket.emit('roomResponse', { response: true, sign: "O" })
+            io.to(id).emit('playerJoin')
+            socket.join(id)
+            io.to(id).emit('current', room.current)
+            io.to(id).emit('getBoard', room.board)
+            room.state = "playing"
+        }
+    })
+
+    socket.on("input", (data) => {
+        let room = rooms.find(r => r.id == data.id)
+        if (room == null) return
+        if (room.current != data.sign) return
+        if (room.state != "playing") return
+        let player = rooms.find(r => r.players.find(p => p.socket.id == socket.id))
+        if (player == null) return
+        let pos = interpret(data.position, room.board)
+        if (room.board[pos[0]][pos[1]] != "") return
+        room.board[pos[0]][pos[1]] = data.sign
+        room.current = room.current == "X" ? "O" : "X"
+        io.to(data.id).emit('current', room.current)
+        io.to(data.id).emit('getBoard', room.board)
+        // check for winner
+        let winner = checkWinner(room.board)
+        if (winner != null) {
+            io.to(data.id).emit('winner', winner)
+            room.state = "finished"
+            //delete room
+            rooms = rooms.filter(r => r.id != data.id)
+        }
+    })
+
+    socket.on('disconnect', () => {
+        let room = rooms.find(r => r.players.find(p => p.socket.id == socket.id))
+        if (room == null) return
+        room.players = room.players.filter(p => p.socket.id != socket.id)
+        //check is room empty
+        if (room.players.length == 0) {
+            rooms = rooms.filter(r => r.id != room.id)
+        }
+        //check is game in progress
+        if (room.state == "playing") {
+            io.to(room.id).emit('leave')
+        }
+    })
 })
+
+function createRoom (room, socket) {
+    return {
+        id: room,
+        players: [{ sign: 'X', socket: socket }],
+        current: "X",
+        board: [["", "", ""], ["", "", ""], ["", "", ""]], // top, middle, bottom
+        state: "waiting"
+    }
+}
+
+function interpret (text, board) {
+    let res = text.split("-")
+    let array = res[0]
+    let pos = res[1]
+    let num1 = null
+    let num2 = null
+    switch (array) {
+        case "top":
+            num1 = 0
+            break;
+        case "middle":
+            num1 = 1
+            break;
+        case "bottom":
+            num1 = 2
+            break;
+    }
+    switch (pos) {
+        case "left":
+            num2 = 0
+            break;
+        case "middle":
+            num2 = 1
+            break;
+        case "right":
+            num2 = 2
+            break;
+    }
+    return [num1, num2]
+}
+
+function checkWinner (board) {
+    let winner = null
+    // check rows
+    if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != "") winner = board[0][0]
+    if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != "") winner = board[1][0]
+    if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != "") winner = board[2][0]
+    // check columns
+    if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != "") winner = board[0][0]
+    if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != "") winner = board[0][1]
+    if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != "") winner = board[0][2]
+    // check diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != "") winner = board[0][0]
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != "") winner = board[0][2]
+    //check is whole board full
+    if (board.flat().filter(p => p == "").length == 0 && winner == null) winner = "draw"
+
+    return winner
+}
---
+++ public/game.js
@@ -0,0 +1,126 @@
+const socket = io()
+const urlParams = new URLSearchParams(window.location.search);
+const room = urlParams.get('id');
+
+let you = ""
+let board = []
+let current = ""
+
+if (room == null) createRoom()
+else joinRoom(room)
+
+function createRoom () {
+    let room = Math.floor(Math.random() * (94323344234123 - 12544523)) + 12544523;
+    room = room.toString().replace(/4/g, 'v').replace(/1/g, 't').replace(/6/g, 'h').replace(/8/g, 'c')
+    window.location.href = `/?id=${room}`
+}
+
+function joinRoom (room) {
+    console.log(`Joining room ${room}`)
+    socket.emit('join', room)
+}
+
+function interpret (text) {
+    let res = text.split("-")
+    let array = res[0]
+    let pos = res[1]
+    let num1 = null
+    let num2 = null
+    switch (array) {
+        case "top":
+            num1 = 0
+            break;
+        case "middle":
+            num1 = 1
+            break;
+        case "bottom":
+            num1 = 2
+            break;
+    }
+    switch (pos) {
+        case "left":
+            num2 = 0
+            break;
+        case "middle":
+            num2 = 1
+            break;
+        case "right":
+            num2 = 2
+            break;
+    }
+    return board[num1][num2]
+}
+
+function sendInput (element) {
+    let position = element.id;
+    if (current != you) return
+    if (interpret(position) != "") return
+    console.log(`${you} placed ${position}`)
+    socket.emit('input', { position, sign: you, id: room })
+}
+
+socket.on("roomResponse", data => {
+    if (data.response == true) {
+        document.querySelector(".state").innerHTML = `Your sign: ${data.sign}`
+        console.log("Joined room!")
+        you = data.sign
+    } else if (data.response == false) {
+        alert("Room is full.")
+    }
+})
+
+socket.on("playerJoin", () => {
+    alert("Player joined, starting game.")
+})
+
+socket.on('current', data => {
+    document.querySelector(".current").innerHTML = `Current player: ${data}`
+    if (data == you) {
+        document.querySelector(".current").innerHTML += ` (You)`
+    }
+    current = data
+})
+
+socket.on('getBoard', data => {
+    function get (id) {
+        return document.getElementById(id)
+    }
+    console.log(data)
+    board = []
+    board = data
+    let tl = get("top-left")
+    let tm = get("top-middle")
+    let tr = get("top-right")
+    let ml = get("middle-left")
+    let mm = get("middle-middle")
+    let mr = get("middle-right")
+    let bl = get("bottom-left")
+    let bm = get("bottom-middle")
+    let br = get("bottom-right")
+    tl.textContent = board[0][0]
+    tm.textContent = board[0][1]
+    tr.textContent = board[0][2]
+    ml.textContent = board[1][0]
+    mm.textContent = board[1][1]
+    mr.textContent = board[1][2]
+    bl.textContent = board[2][0]
+    bm.textContent = board[2][1]
+    br.textContent = board[2][2]
+})
+
+socket.on("winner", data => {
+    if (data == "draw") alert("It's a draw!")
+    else alert(`${data} won!`)
+    let pr = prompt("Rematch? (y/n)")
+    if (pr == "y") {
+        window.location.reload()
+    } else {
+        alert("Thanks for playing!")
+        window.location.href = "/"
+    }
+})
+
+socket.on("leave", () => {
+    alert("Other player left the room. Reseting.")
+    window.location.href = "/"
+})
